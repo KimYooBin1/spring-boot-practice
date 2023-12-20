e07b0443ff9ee8dc9a9694b18dcbbdec922e4218
KimYooBin1 <kyb1208tg@gmail.com>
2023-12-20 15:31:36 +0900

    mvc-template

--- src/main/java/hello/springhello/controller/HelloController.java
+++ src/main/java/hello/springhello/controller/HelloController.java
@@ -3,12 +3,20 @@ package hello.springhello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")    //get post : hello 를 받는다
+    @GetMapping("hello")    //get post : /hello 을 인식한다
     public String hello(Model model){
-        model.addAttribute("data", "hello!!");
-        return "hello";     //resource templates 에서 hello를 가진 파일을 실행한다
+        model.addAttribute("data", "hello!!");  //key는 data, value 는 hello!!
+        return "hello";     //resource templates 에서 hello를 가진 파일을 실행시켜라
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
